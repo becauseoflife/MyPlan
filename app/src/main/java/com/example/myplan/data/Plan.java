d134aadaf33f205f25fb1ecd6a39f855d3ba7cf4
becauseoflife <1752196851@qq.com>
2019-11-08 01:01:43 +0800

    添加初步拟定数据的类和主页item的xml文件

---
+++ app/src/main/java/com/example/myplan/data/Plan.java
@@ -0,0 +1,121 @@
+package com.example.myplan.data;
+
+public class Plan {
+    private String title;       // 标题
+    private String remarks;     // 备注信息
+    private int backgroundImg;  // 背景图
+    private int label;          // 标签
+    private int cycleTime;      // 重复设置
+
+    private int year;       // 年
+    private int month;      // 月
+    private int day;        // 日
+    private int hour;       // 时
+    private int minute;     // 分
+    private int second;     // 秒
+
+
+    public Plan(String title, String remarks, int backgroundImg, int label, int cycleTime, int year, int month, int day, int hour, int minute, int second) {
+        this.title = title;
+        this.remarks = remarks;
+        this.backgroundImg = backgroundImg;
+        this.label = label;
+        this.cycleTime = cycleTime;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public int getBackgroundImg() {
+        return backgroundImg;
+    }
+
+    public void setBackgroundImg(int backgroundImg) {
+        this.backgroundImg = backgroundImg;
+    }
+
+    public int getLabel() {
+        return label;
+    }
+
+    public void setLabel(int label) {
+        this.label = label;
+    }
+
+    public int getCycleTime() {
+        return cycleTime;
+    }
+
+    public void setCycleTime(int cycleTime) {
+        this.cycleTime = cycleTime;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+}
